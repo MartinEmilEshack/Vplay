c38af850d3ae44b4f76648d37befbe06741f9851
Martin Emil <mar10martinemil@gmail.com>
2020-06-03 04:16:27 +0200

    Vplay can now upload videos by an admin

--- vplay_web/settings.py
+++ vplay_web/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'vsrc')
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     
     # Vplay Apps
     'home',
+    'videos',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/vsrc/'

1116fa6f3b99ec586554728e66f23b9e989eb17f
Martin Emil <mar10martinemil@gmail.com>
2020-06-03 04:27:47 +0200

    Added Media_url after git conflict

--- vplay_web/settings.py
+++ vplay_web/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/assets/'
+MEDIA_URL = '/vsrc/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
